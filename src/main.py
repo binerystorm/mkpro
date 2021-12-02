b92196d6688d38c512a8173b85cccec8e2e5ed5d
binerystorm <gerick.de.kruyf123@gmail.com>
2021-12-02 17:27:45 +0100

    added some new flags for command line

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@ import sys
 import os
 from typing import Final
 
+# Global vars
 CLI_CMDS: list[str] = ["lib", "pro"]
+TEMP_FILE: str = ""
 
 def prompt() -> int:
     pass
 
 def str2path(name: str, *args: str) -> list[str]:
-    full_path: list[str] = f"{os.getcwd()}/{name}"
+    full_path: str = f"{os.getcwd()}/{name}"
 
     if not os.access(full_path, os.F_OK):
         return list(map(lambda x: full_path, args))
@@ -16,33 +18,33 @@ def str2path(name: str, *args: str) -> list[str]:
         print(f"error! directory/file {name} already exists", file=sys.stderr)
         exit(1)
 
-def parse_cli() -> tuple[str, list[str]]:
+def parse_cli() -> tuple[list[str], list[str]]:
     index: int = 0
     name: str = ""
-    temp_file: str = ""
-    paths: list[str] = ["src/", "test/", "doc/"]
+    dirs: list[str] = ["src/", "test/", "doc/"]
+    files: list[str] = []
 
     # check if mkpro has flags
-    if sys.argv[index + 1] not in CLI_CMDS:
+    try:
+        if sys.argv[index + 1] not in CLI_CMDS:
 
-        # parse mkpro flags
-        try:
-            while((sys.argv[index + 1] not in CLI_CMDS) or \
-                  (sys.argv[index] not in CLI_CMDS)):
-                if sys.argv[index] == "-t":
+            # parse mkpro flags
+                while((sys.argv[index + 1] not in CLI_CMDS) or \
+                      (sys.argv[index] not in CLI_CMDS)):
+                    if sys.argv[index] == "-t":
+                        index += 1
+                        TEMP_FILE = sys.argv[index]
+                        continue
+                    else:
+                        print(f"error! unknown flag '{sys.argv[index]}' for mkpro", file=sys.stderr)
+                        exit(1)
                     index += 1
-                    temp_file = sys.argv[index]
-                    continue
-                else:
-                    print(f"error! unknown flag '{sys.argv[index]}' for mkpro", file=sys.stderr)
-                    exit(1)
                 index += 1
+        else:
             index += 1
-        except IndexError:
-            print("error! command not given", file=sys.stderr)
-            exit(1)
-    else:
-        index += 1
+    except IndexError:
+        print("error! command not given", file=sys.stderr)
+        exit(1)
 
     # parse cmd & its flags
     assert len(CLI_CMDS) == 2, "error! possible unhandled command (dev)"
@@ -59,27 +61,42 @@ def parse_cli() -> tuple[str, list[str]]:
 
         while (index := index + 1) < len(sys.argv):
             if sys.argv[index] == "-b":
-                paths.append("bin/")
+                dirs.append("bin/")
                 continue
-            elif sys.argv[index] == "-r":
-                paths.append("README.md")
-            elif sys.argv[index] == "-R":
+            elif sys.argv[index] == "-d":
+                files.append("README.md")
+                continue
+            elif sys.argv[index] in ("-r", "-R"):
                 try:
                     index += 1
-                    if sys.argv[index] in paths:
-                        paths.remove(sys.argv[index])
+                    if (f"{sys.argv[index]}/" in dirs) or \
+                       (sys.argv[index] in files):
+                        if sys.argv[index - 1] == "-r":
+                            print(f"{sys.argv[index]}/")
+                            dirs.remove(f"{sys.argv[index]}/")
+                            continue
+                        else:
+                            files.remove(f"{sys.argv[index]}")
+                            continue
+                            
                     else:
                         # NOTE: might want to continue program even if file doesn't exist
-                        print("error! default directory to remove does not exist", file=sys.stderr)
+                        print(f"error! default directory/file {sys.argv[index]} to remove does not exist", file=sys.stderr)
                         exit(1)
                 except IndexError:
-                    print("error! file to be removed was not given", file=sys.stderr)
+                    print("error! directory/file to be removed was not given", file=sys.stderr)
                     exit(1)
-            elif sys.argv[index] == "-n":
+            elif sys.argv[index] in ("-n", "-N"):
                 try:
                     index += 1
-                    if sys.argv[index] not in paths:
-                        paths.append(sys.argv[index])
+                    if (f"{sys.argv[index]}" not in dirs) or \
+                       (sys.argv[index] not in files):
+                        if sys.argv[index - 1] == "-n":
+                            dirs.append(f"{sys.argv[index]}/")
+                            continue
+                        else:
+                            files.append(f"{sys.argv[index]}")
+                            continue
                     else:
                         # NOTE: might want to continue program even if file already exists
                         print("error! directory to be added already exists", file=sys.stderr)
@@ -94,19 +111,20 @@ def parse_cli() -> tuple[str, list[str]]:
             index += 1
 
     # pathefy list
-    paths = str2path(name, *paths)
     
-    return temp_file, paths
-                        
+    return dirs, files
 
-def build_path(path: str) -> int:
-    pass
+# def build_path(path: str) -> int:
+#     if path[-1] != '/':
+#         os.makedirs(path)
+#     else:
+#         pass
+#     return 1
 
 def main() -> None:
-    n, t, p = parse_cli()
-    print(n)
-    print(t)
-    print(p)
+    f, d = parse_cli()
+    print(f)
+    print(d)
 
 if __name__ == "__main__":
     main()
